bf7759ffaa4006372d0997d9372697614312a4e4
MatievisTheKat <matievisthekat@gmail.com>
2020-10-26 12:58:41 +0200

    feat(permissions): roles required to use deny and accept commands

--- src/commands/accept.ts
+++ src/commands/accept.ts
@@ -3,6 +3,9 @@ import { Message, MessageEmbed } from "discord.js";
 export default {
 	name: "accept",
 	run: async (msg: Message, args: string[]) => {
+		if (!msg.member?.roles.cache.has("756615437243908267") && !msg.member?.roles.cache.has("769870496653115427"))
+			return await msg.channel.send("You do not have the requried role to use this");
+
 		const target = msg.mentions.users.first() || msg.client.users.cache.get(args[0]);
 		if (!target)
 			return await msg.channel.send("Target not found. Make sure to provide a valid user mention or ID");
